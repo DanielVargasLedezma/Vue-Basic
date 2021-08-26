880ff248cb6d72f4e643141e03bfcf8248e7baa2
Dan_VL <danilevl229@gmail.com>
2021-08-26 03:17:03 -0600

    First commit

---
+++ src/components/header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header
+    id="header"
+    class="d-flex flex-row justify-content-between align-items-center header"
+  >
+    <div class="col-md-3 col-sm-3 col-3">
+      <section
+        class="
+          d-flex
+          flex-row
+          justify-content-start
+          align-items-center
+          icono-titulo
+        "
+      >
+        <div class="col-md-4 col-sm-4 col-4 icono">
+          <img src="../assets/logo.png" class="app-logo" alt="logotipo" />
+        </div>
+        <div class="col-md-4 col-sm-4 col-4 titulo">
+          <h1>Vue.js</h1>
+        </div>
+      </section>
+    </div>
+    <div class="col-md-5 col-sm-5 col-5"></div>
+    <div class="col-md-4 col-sm-4 col-4 enlaces">
+      <ul class="d-flex flex-row justify-content-end navbar">
+        <li class="col-md-3 col-sm-6 col-6 enlace">
+          <a href="#"> Inicio </a>
+        </li>
+        <li class="col-md-3 col-sm-6 col-6 enlace">
+          <a href="#"> Bot </a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<style>
+@import "../assets/css/styles.css";
+</style>
